f6641db21017959c3d6caa391b42f771b431dcd1
Ivan <alexandre.raymond@etu.u-pec.fr>
2020-10-17 11:15:14 +0200

    Added Physic from Eren file

--- l3/lemmings/App.java
+++ l3/lemmings/App.java
@@ -14,11 +14,20 @@ public class App {
 
 	public final static int WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
 	public final static int HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
-	
+	private final static int    MAX_FPS = 50;
+	// maximum number of frames to be skipped
+	private final static int    MAX_FRAME_SKIPS = 5;
+	// the frame period
+	private final static int    FRAME_PERIOD = 30000 / MAX_FPS;
+
 	public static void main(String[] args) throws InterruptedException {
 		ArrayList<Lemming> lemmings = new ArrayList<Lemming>();
 		ArrayList<Block> blocks = new ArrayList<Block>();
-		Views view = new Views(new Models(),WIDTH-200, HEIGHT-100, lemmings, blocks);
+		int numCaseX = 40;
+		int numCaseY = 24;
+
+
+		Views view = new Views(new Models(),WIDTH-200, HEIGHT-100, lemmings, blocks, numCaseX, numCaseY);
 		JFrame frame = new JFrame("Lemmingway");
 		frame.add(view);
 		frame.setSize(view.getSize());
@@ -26,16 +35,29 @@ public class App {
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
-		lemmings.add(new Lemming(10,15));
+
 		for(int i = 10; i<25;i++)
+		{
 			blocks.add(new Block(i,16));
+
+		}
+		blocks.add(new Block(10,14));
+		blocks.add(new Block(10,15));
+		blocks.add(new Block(17,15));
+		blocks.add(new Block(18,14));
+		blocks.add(new Block(19,13));
+		blocks.add(new Block(24,14));
+		blocks.add(new Block(24,15));
+		lemmings.add(new Lemming(11,15));
+
+
 		while (true) {
 			view.update();
 			view.repaint();
 			Thread.sleep(900);
+			//Thread.sleep(100);
+
 		}
-		
 	}
 
 }
