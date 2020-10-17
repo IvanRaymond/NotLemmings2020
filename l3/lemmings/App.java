690d24b265e32cafe0e31e48f60769e9b68837dc
Ivan <alexandre.raymond@etu.u-pec.fr>
2020-10-17 12:42:26 +0200

    Removed comments

--- l3/lemmings/App.java
+++ l3/lemmings/App.java
@@ -1,14 +1,9 @@
 package l3.lemmings;
 
 import java.awt.Toolkit;
-import java.util.ArrayList;
-
 import javax.swing.JFrame;
 
-import l3.lemmings.model.Block;
-import l3.lemmings.model.Lemming;
 import l3.lemmings.model.Level;
-import l3.lemmings.model.Models;
 import l3.lemmings.view.Views;
 
 public class App {
@@ -23,14 +18,8 @@ public class App {
 
 	public static void main(String[] args) throws InterruptedException {
 
-
-		ArrayList<Lemming> lemmings = new ArrayList<Lemming>();
-		ArrayList<Block> blocks = new ArrayList<Block>();
-
-
 		int numCaseX = 40;
 		int numCaseY = 24;
-
 
 		Views view = new Views(new Level(),WIDTH-200, HEIGHT-100, numCaseX, numCaseY);
 		JFrame frame = new JFrame("Lemmingway");
@@ -41,28 +30,10 @@ public class App {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 
-
-//		for(int i = 10; i<25;i++)
-//		{
-//			blocks.add(new Block(i,16));
-//
-//		}
-//		blocks.add(new Block(10,14));
-//		blocks.add(new Block(10,15));
-//		blocks.add(new Block(17,15));
-//		blocks.add(new Block(18,14));
-//		blocks.add(new Block(19,13));
-//		blocks.add(new Block(24,14));
-//		blocks.add(new Block(24,15));
-//		lemmings.add(new Lemming(10,15));
-
-
 		while (true) {
 			view.update();
 			view.repaint();
 			Thread.sleep(900);
-			//Thread.sleep(100);
-
 		}
 	}
 
