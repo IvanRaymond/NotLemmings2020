58bb8d0baa0e7e6b93b46d18e3f6f335d938c644
e <niumist+github@pm.me>
2021-01-07 17:33:20 +0100

    Fixed Bomber

--- l3/lemmings/observable/Level.java
+++ l3/lemmings/observable/Level.java
@@ -8,6 +8,8 @@ import l3.lemmings.observable.props.Entrance;
 import l3.lemmings.observable.props.Staircase;
 
 import java.awt.*;
+import java.lang.annotation.ElementType;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 //import org.json.simple.JSONArray;
@@ -267,11 +269,11 @@ public class Level {
     }
 
     public void destroySurrounding(Point point, int range){
-        for(int i = 0; i < elements.size(); i++){
-            Point p = elements.get(i).getPosition();
+        for(IElement current : new ArrayList<IElement>(elements)){
+            Point p = current.getPosition();
             if (p.getX() <= point.getX() + range && p.getX() >= point.getX() - range &&
                     p.getY() >= point.getY() - range && p.getY() <= point.getY() + range) {
-                removeElement(elements.get(i).getPosition());
+            removeElement(current.getPosition());
             }
         }
     }
